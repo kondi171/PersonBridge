2f311fe41c712d5938fd76dcd0497e13b0b09520
Konrad Nowak <wk.k.nowak@gmail.com>
2024-05-13 00:56:00 +0200

    implement multi-threading server

--- client/src/app/app.environment.ts
+++ client/src/app/app.environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:4000/v1',
-  serverURL: 'http://localhost:4000'
+  serverURL: 'http://localhost:4616'
 };
--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,9 @@ import cors from "cors";
 import session from "express-session";
 import MongoStore from "connect-mongo";
 
+import cluster from 'cluster';
+import { cpus } from 'os';
+
 import userRoutes from "./routes/users.routes";
 import authenticationRoutes from "./routes/authentication.routes";
 import accessRoutes from "./routes/access.routes";
@@ -11,52 +14,68 @@ import exploreRoutes from "./routes/explore.routes";
 
 import startSocketServer from "./middlewares/websocket.middleware";
 
-const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 4000;
-const app = express();
+const numCPUs = cpus().length;
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
 
-const username: string = "WebKing";
-const password: string = "gJKkzxRt1toy7TCr";
-const cluster: string = "cluster0.niyeq92";
-const dbname: string = "PersonBridge";
+  // Fork workers
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-mongoose.connect(`mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}`);
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
 
-const db = mongoose.connection;
-const corsOptions = {
-  origin: 'http://localhost:4200',
-  credentials: true,
-};
+  const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 4616;
+  const app = express();
 
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
-});
+  const username: string = "WebKing";
+  const password: string = "gJKkzxRt1toy7TCr";
+  const clusterURL: string = "cluster0.niyeq92";
+  const dbname: string = "PersonBridge";
 
-app.use(express.json({ limit: '50mb' }));
-app.use(cors(corsOptions));
-app.use(express.urlencoded({
-  extended: true,
-  limit: '50mb',
-}));
+  mongoose.connect(`mongodb+srv://${username}:${password}@${clusterURL}.mongodb.net/${dbname}`);
+  // mongoose.connect(`mongodb+srv://${username}:${password}@${clusterURL}/${dbname}`);
 
-// Session configuration
-app.use(session({
-  secret: '61dc8cfcaee2ff7da7203c4aec19d6c509f9fe16bb23d2b44ae1cced5591e767',
-  resave: false,
-  saveUninitialized: true,
-  store: MongoStore.create({
-    mongoUrl: `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}`
-  }),
-  cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 } // 24 hours
-}));
+  const db = mongoose.connection;
+  const corsOptions = {
+    origin: 'http://localhost:4200',
+    credentials: true,
+  };
 
-app.use(userRoutes);
-app.use(authenticationRoutes);
-app.use(accessRoutes);
-app.use(exploreRoutes);
+  db.on("error", console.error.bind(console, "connection error: "));
+  db.once("open", function () {
+    console.log("Connected successfully");
+  });
 
-const server = startSocketServer(app);
+  app.use(express.json({ limit: '50mb' }));
+  app.use(cors(corsOptions));
+  app.use(express.urlencoded({
+    extended: true,
+    limit: '50mb',
+  }));
 
-server.listen(PORT, () => {
-  console.log(`Server is running at port ${PORT}`);
-});
+  // Session configuration
+  app.use(session({
+    secret: '61dc8cfcaee2ff7da7203c4aec19d6c509f9fe16bb23d2b44ae1cced5591e767',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: `mongodb+srv://${username}:${password}@${clusterURL}.mongodb.net/${dbname}`
+    }),
+    cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 } // 24 hours
+  }));
+
+  app.use(userRoutes);
+  app.use(authenticationRoutes);
+  app.use(accessRoutes);
+  app.use(exploreRoutes);
+
+  const server = startSocketServer(app);
+
+  server.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+  });
+}
